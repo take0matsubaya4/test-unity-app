75990c834a7f49077b7b70c91518272e824f8837
take0matsubaya4 <takeo.i.matsubayashi@gmail.com>
2017-01-12 15:16:08 +0900

    Created unity project.

---
+++ Assets/MessageTableViewCell.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// リスト項目のデータクラスを定義
+public class MessageData
+{
+	public string iconName;		// アイコン名
+	//public float height;		// 送信者名
+	public string name;			// 送信者名
+	public string message;		// メッセージ
+	public string date;			// 送信日時
+}
+
+// TableViewCell<T>クラスを継承する
+public class MessageTableViewCell : TableViewCell<MessageData>
+{
+	[SerializeField] private Image iconImage;	// アイコンを表示するイメージ
+	//[SerializeField] private GameObject cellContent;	// セルの内容
+	[SerializeField] private Text nameLabel;	// 送信者名を表示するテキスト
+	[SerializeField] private Text messageText;	// メッセージを表示するテキスト
+	[SerializeField] private Text dateLabel;	// 送信日時を表示するテキスト
+
+	// セルの内容を更新するメソッドのオーバーライド
+	public override void UpdateContent(MessageData MessageData)
+	{
+		nameLabel.text = MessageData.name;
+		messageText.text = MessageData.message;
+		dateLabel.text = MessageData.date;
+		//MessageData.height = cellContent.GetComponent<RectTransform>().sizeDelta.y;
+
+		#region アイコンのスプライトを変更するコードの追加
+		// スプライトシート名とスプライト名を指定してアイコンのスプライトを変更する
+		iconImage.sprite = 
+			SpriteSheetManager.GetSpriteByName("IconAtlas", MessageData.iconName);
+		#endregion
+	}
+}
---
+++ Assets/ShopItemTableViewCell.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// リスト項目のデータクラスを定義
+public class ShopItemData
+{
+	public string iconName;		// アイコン名
+	public string name;			// アイテム名
+	public int price;			// 価格
+	public string description;	// 説明
+}
+
+// TableViewCell<T>クラスを継承する
+public class ShopItemTableViewCell : TableViewCell<ShopItemData>
+{
+	[SerializeField] private Image iconImage;	// アイコンを表示するイメージ
+	[SerializeField] private Text nameLabel;	// アイテム名を表示するテキスト
+	[SerializeField] private Text priceLabel;	// 価格を表示するテキスト
+
+	// セルの内容を更新するメソッドのオーバーライド
+	public override void UpdateContent(ShopItemData itemData)
+	{
+		nameLabel.text = itemData.name;
+		priceLabel.text = itemData.price.ToString();
+
+#region アイコンのスプライトを変更するコードの追加
+		// スプライトシート名とスプライト名を指定してアイコンのスプライトを変更する
+		iconImage.sprite = 
+			SpriteSheetManager.GetSpriteByName("IconAtlas", itemData.iconName);
+#endregion
+	}
+}
